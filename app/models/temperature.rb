64a3d87e59fed7134a33217032716a14a9e5ed76
Mike Kijewski <mikekijewski@Mikes-MacBook-Pro.local>
2017-08-29 12:24:42 -0700

    added some temp analysis functions

--- app/models/temperature.rb
+++ app/models/temperature.rb
@@ -6,4 +6,24 @@ class Temperature < ApplicationRecord
     return temperature_f
   end
 
+  def corresponding_temp
+    temp_2 = Temperature.where("created_at < ? AND thermometer_id = ?", self.created_at + 1.minute, 2).order(created_at: :desc).first
+    return temp_2
+  end
+
+  def get_adjacent_temp_range(interval)
+    later_temp = Temperature.where("thermometer_id = ? AND created_at > ?", self.id, self.created_at + (interval - 1).minute).order(created_at: :asc).first
+    earlier_temp = Temperature.where("thermometer_id = ? AND created_at < ?", self.id, self.created_at - (interval + 1).minute).order(created_at: :desc).first
+    if !later_temp.nil? and !earlier_temp.nil?
+      return later_temp.temperature_f - earlier_temp.temperature_f
+    else
+      return nil
+    end
+  end
+
+  def self.find_slope(outside_temp,inside_temp,inside_temp_range)
+    slope = inside_temp_range.to_f / (outside_temp.temperature_f.to_f - inside_temp.temperature_f.to_f)
+    return slope
+  end
+
 end
